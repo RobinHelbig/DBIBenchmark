5d2a339e0f389ffbb3a6495080ea3688f29c36df
Robin Helbig <helbig67@web.de>
2019-12-06 17:33:13 +0100

    Init

---
+++ src/main/java/dbi25/benchmark/Main.java
@@ -0,0 +1,19 @@
+package dbi25.benchmark;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        // config
+        final String server = "JonasVM";
+        final String instance = "DBISQL";
+        final String database = "benchmark";
+        final String user = "dbi";
+        final String password = "dbi_pass";
+
+        // benchmark
+        InsertBenchmark.insertBenchmark(10, server, instance, database, user, password);
+        InsertBenchmark.insertBenchmark(20, server, instance, database, user, password);
+        InsertBenchmark.insertBenchmark(50, server, instance, database, user, password);
+    }
+}
